300c1b5c16ec6e46ef39fae63a761d3623f5210d
mignot <andreas.garcia@bodzify.com>
2024-06-06 18:21:36 +0200

    Fix

--- src/loader/Sample.py
+++ src/loader/Sample.py
@@ -10,5 +10,10 @@ class Sample:
         self.released_on = released_on
         self.url = url
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+    def _to_camel_case(self, snake_str):
+        components = snake_str.split('_')
+        return components[0] + ''.join(x.title() for x in components[1:])
+
+    def to_json(self):
+        obj_dict = {self._to_camel_case(k): v for k, v in self.__dict__.items()}
+        return json.dumps(obj_dict, sort_keys=True)
